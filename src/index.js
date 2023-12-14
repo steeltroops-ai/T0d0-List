f9c6c82725e5eb9ed8d6c3a0f8895a81243287ff
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2023-12-14 16:07:39 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"
-import App from "./Calculator";
 
-
-
-const App = () => {
-  const [showApp, setShowApp] = useState(false);
-  const [showCalculator, setShowCalculator] = useState(false);
-
-  const handleButtonClick = (component) => {
-    if (component === "App") {
-      setShowApp(true);
-      setShowCalculator(false);
-    } else if (component === "Calculator") {
-      setShowApp(false);
-      setShowCalculator(true);
-    }
-  };
-
-  return (
-    <React.StrictMode>
-      <button onClick={() => handleButtonClick("App")}>Show App</button>
-      <button onClick={() => handleButtonClick("Calculator")}>Show Calculator</button>
-      {showApp && <App />}
-      {showCalculator && <Calculator />}
-    </React.StrictMode>
-  );
-};
-
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
