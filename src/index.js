c10a98011154c5b4c8c1c64d6f3005687a1e3669
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2023-12-14 15:58:25 +0530

    .....
    ..

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"
+import App from "./Calculator";
+
+const ToggleComponent = () => {
+  const [isAppVisible, setIsAppVisible] = useState(true);
+
+  const toggleComponent = () => {
+    setIsAppVisible(!isAppVisible);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleComponent}>Toggle Component</button>
+      {isAppVisible ? <App /> : <Calculator />}
+    </div>
+  );
+};
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+const App = () => {
+  const [showApp, setShowApp] = useState(false);
+  const [showCalculator, setShowCalculator] = useState(false);
+
+  const handleButtonClick = (component) => {
+    if (component === "App") {
+      setShowApp(true);
+      setShowCalculator(false);
+    } else if (component === "Calculator") {
+      setShowApp(false);
+      setShowCalculator(true);
+    }
+  };
+
+  return (
+    <React.StrictMode>
+      <button onClick={() => handleButtonClick("App")}>Show App</button>
+      <button onClick={() => handleButtonClick("Calculator")}>Show Calculator</button>
+      {showApp && <App />}
+      {showCalculator && <Calculator />}
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
 );
-
 
