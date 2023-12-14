ac55c6e280e199dbf4bb8f8bc9731e2d49561628
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2023-12-14 15:25:27 +0530

    ...

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -9,6 +9,8 @@ export const EditTodoForm = ({editTodo, task}) => {
         // edit todo
         editTodo(value, task.id);
       };
+
+      
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
     <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
